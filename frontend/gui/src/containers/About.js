176c93937007c042cced61816ef0ddcc5c78e27b
Emilio <erogalla@utexas.edu>
2019-12-26 22:59:26 -0600

    updated about cleared superuser and deleted projects

--- frontend/gui/src/containers/About.js
+++ frontend/gui/src/containers/About.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
+
 const About = () => {
     return (
         <div className="row">
             <div className="col-xl-10">
                 <div className="content-column-content">
-                    <h1>About me</h1>
-                    <p className="lead">An sincerity so extremity he additions. Her yet <strong>there truth merit</strong>. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly. Off can admiration prosperous now devonshire diminution law.</p>
+                    <h1>About me!</h1>
                     <div className="row">
                         <div className="col-lg-8">
-                            <p>Received overcame oh sensible so at an. Formed do change merely to county it. <strong>Am separate contempt</strong> domestic to to oh. On relation my so addition branched. Put hearing cottage she norland letters equally prepare too. Replied exposed savings he no viewing as up. Soon body add him hill. No father living really people estate if. Mistake do produce beloved demesne if am pursuit.</p>
-                            
+                            <p className="lead">I am a first year Computer Science student at the University of Texas at Austin! I went to North-Garland Highschool and graduated top 5% of my school.
+                            I learned Java through professional programming classes in School. I taught myself the Python, Javascript, and HTML/CSS languages. I am currently learning the Django Framework and Tensorflow.   </p>
                         </div>
                         <div className="col-md-4">
-                            <h1>Some image></h1>
+                            <img src="emilio.jpeg" alt="Profile" />
                         </div>
                     </div>
-                    <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
                     <h4 className="text-uppercase">Education</h4>
+                    <p>Current Classes</p>
                     <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
+                        <li>Computer Organization and Architecture</li>
                     </ul>
-                    <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then</p>
+                    <p>Previously Classes</p>
+                    <ul>
+                        <li>Data Structures</li>
+                        <li>Discreet Mathematics</li>
+                        <li>AP Computer Science</li>
+                    </ul>
                 </div>
             </div>
         </div>
