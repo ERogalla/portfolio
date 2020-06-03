951fa41440142e9031aa88c9c341e40899037a8b
ERogalla <erogalla@utexas.edu>
2020-06-02 20:34:54 -0500

    testing deployment

--- backend/src/personal/settings/production.py
+++ backend/src/personal/settings/production.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
 from .base import *
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+secret_info = json.load(open(os.path.join(os.path.dirname(BASE_DIR), 'src', 'secret.json')))
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['www.emilior.com', 'emilior.com', '18.222.171.163']
@@ -8,9 +12,9 @@ ALLOWED_HOSTS = ['www.emilior.com', 'emilior.com', '18.222.171.163']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'testproject',
-        'USER': 'testprojectuser',
-        'PASSWORD': 'password',
+        'NAME': secret_info['dbname'], 
+        'USER': secret_info['dbuser'], 
+        'PASSWORD': secret_info['dbpassword'], 
         'HOST': 'localhost',
         'PORT': '',
     }
