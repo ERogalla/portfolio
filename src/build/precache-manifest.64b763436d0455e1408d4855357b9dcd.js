6a7e8bfede12d61f4e28bae0fba28ced1c812bcd
ERogalla <erogalla@utexas.edu>
2020-06-05 22:07:28 -0500

    moving files around

---
+++ src/build/precache-manifest.64b763436d0455e1408d4855357b9dcd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d5271814c1fdd6aa948b581d81ae81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d8e67cea4a8d08e399b",
+    "url": "/static/css/main.7848c0d7.chunk.css"
+  },
+  {
+    "revision": "18c255c1328f597fc329",
+    "url": "/static/js/2.c001fb52.chunk.js"
+  },
+  {
+    "revision": "57dfa445a0b4c41cf322086323229659",
+    "url": "/static/js/2.c001fb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8e67cea4a8d08e399b",
+    "url": "/static/js/main.4f075d2d.chunk.js"
+  },
+  {
+    "revision": "c1c7c5cf504a70063ac5",
+    "url": "/static/js/runtime-main.a857e0b8.js"
+  },
+  {
+    "revision": "74a85b35b83c23c8e290cc0b28e1b63f",
+    "url": "/static/media/emilio.74a85b35.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ src/projects/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import ProjectListView, ProjectDetailView
+
+urlpatterns = [
+    path('', ProjectListView.as_view()), 
+    path('<slug>/', ProjectDetailView.as_view()) 
+]   
